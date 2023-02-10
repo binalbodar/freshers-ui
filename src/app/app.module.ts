b39a19ee7a98a42b7c8a15a912f97c51c125bcdb
ashish-adishtech <ashish@adistech.in>
2023-02-10 09:13:57 +0530

    api

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AuthInterceptor } from '../app/services/auth/auth.interceptor';
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'en-GB' },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+   BaseUrl:'http://localhost:3000/api/'
+  // BaseUrl:'https://apin.pb.diamonds:9801/api/'
+}
